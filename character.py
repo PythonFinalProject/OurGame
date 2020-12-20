02c75f97c4b8e81d2b2ef8762b2a899061f13092
NelsonLee0124 <a06w.68577818@gmail.com>
2020-12-21 04:00:13 +0800

    Add multiplyer and collision check

---
+++ character.py
@@ -0,0 +1,151 @@
+import pygame
+import random
+
+CREATE_ENEMY_EVENT = pygame.USEREVENT
+
+class Character():
+    def __init__(self, x, y, width, height):
+        self.x = x
+        self.y = y
+        self.width = width
+        self.height = height
+        self.vel = 3
+        self.health = 100
+        self.up = False
+        self.down = False
+        self.left = False
+        self.right = False
+        self.walk_up = []
+        self.walk_down = []
+        self.walk_left = []
+        self.walk_right = []
+        self.walk_pause = None
+        self.walkcount = 0
+    
+    def set_hitbox(self, x_offset, y_offset, width, height):
+        self.hitbox = (self.x + x_offset, self.y + y_offset, width, height)
+
+    def extract_from_sprite_sheet(self, image_source, dir, steps):
+        sprite_sheet = pygame.image.load(image_source)
+        self.walk_sheet = [self.walk_up, self.walk_left, self.walk_down, self.walk_right]
+        for i in range(dir):
+            for  j in range(steps):
+                self.walk_sheet[i].append(sprite_sheet.subsurface((self.width*j, self.height*i, self.width, self.height)))
+        self.walk_pause = self.walk_sheet[2][0]
+    
+    def draw(self, win, frames=3):
+        if self.walkcount + 1 >= frames*9:
+            self.walkcount = 0
+        if self.left:
+            win.blit(self.walk_left[self.walkcount//frames], (self.x, self.y))
+            self.walkcount += 1
+        elif self.right:
+            win.blit(self.walk_right[self.walkcount//frames], (self.x, self.y))
+            self.walkcount += 1
+        elif self.up:
+            win.blit(self.walk_up[self.walkcount//frames], (self.x, self.y))
+            self.walkcount += 1
+        elif self.down:
+            win.blit(self.walk_down[self.walkcount//frames], (self.x, self.y))
+            self.walkcount += 1
+        else:
+            win.blit(self.walk_pause, (self.x, self.y))
+
+class Player(Character):
+    def __init__(self, x, y, width, height):
+        super().__init__(x, y, width, height)
+        self.set_hitbox(15, 5, 594//9 - 35, 261//4 - 10)
+        self.extract_from_sprite_sheet('Game/blue_woman_sprite.png', 4, 9)
+        self.player_selection = {"1P": {"left": pygame.K_a, "right": pygame.K_d, "up": pygame.K_w, "down": pygame.K_s}, "2P": {"left": pygame.K_LEFT, "right": pygame.K_RIGHT, "up": pygame.K_UP, "down": pygame.K_DOWN}}
+
+    def control(self, run, win_width, win_height, num_player):
+        keys = pygame.key.get_pressed()
+        player_control = self.player_selection[num_player]
+
+        if keys[player_control["left"]] and self.x > 0:
+            self.x -= self.vel
+            self.left = True
+            self.right = False
+        elif keys[player_control["right"]] and self.x < win_width - self.width:
+            self.x += self.vel
+            self.right = True
+            self.left = False
+        if keys[player_control["up"]] and self.y > 0:
+            self.y -= self.vel
+            self.up = True
+            self.down = False
+        elif keys[player_control["down"]] and self.y < win_height - self.height:
+            self.y += self.vel
+            self.down = True
+            self.up = False
+        if keys[player_control["left"]] == 0 and keys[player_control["right"]] == 0 and keys[player_control["up"]] == 0 and keys[player_control["down"]] == 0:
+            self.right = False
+            self.left = False
+            self.down = False
+            self.up = False
+            self.walkcount = 0
+        if keys[pygame.K_q]:
+            run[0] = False
+
+    def draw(self, win, frames=3):
+        super().draw(win, frames)
+        self.set_hitbox(15, 5, 594//9 - 35, 261//4 - 10)
+        pygame.draw.rect(win, (255, 0, 0), self.hitbox, 2)
+
+    def hit(self):
+        self.health -= 1
+        print(self.health)
+        # font1 = pygame.font.SysFont('comicsans', 100)
+        # text = font1.render("hit", 1, (255, 0, 0))
+        # # print(self.health)
+        # win.blit(text, ((win_width - text.get_width()) // 2, (win_height - text.get_height()) // 2))
+        # pygame.display.update()
+# 576*256
+class Enemy(Character):
+    def __init__(self, x, y, width, height):
+        super().__init__(x, y, width, height)
+        self.target = None
+        self.set_hitbox(15, 10, 576//9 - 35, 256//4 - 10)
+        self.extract_from_sprite_sheet('Game/skull_sprite.png', 4, 9)
+        self.vel = 3
+        pygame.time.set_timer(CREATE_ENEMY_EVENT, 800) # Create enemy every 1 sec
+    
+    def chase(self, player):
+        dx = (player.x - self.x)# + random.randrange(-200, 200, 1)
+        dy = (player.y - self.y)# + random.randrange(-200, 200, 1)
+        dl = (dx**2 + dy**2)**0.5
+        self.x += self.vel*dx/dl
+        self.y += self.vel*dy/dl
+        
+        if dx > 0:
+            self.right = True
+            self.left = False
+        else:
+            self.left = True
+            self.right = False
+        if dy > 0:
+            self.down = True
+            self.up = False
+        else:
+            self.up = True
+            self.down = False
+
+        if abs(dx) > abs(dy):
+            self.up = False
+            self.down = False
+        else:
+            self.right = False
+            self.left = False
+
+        if dx == 0 and dy == 0:
+            self.right = False
+            self.left = False
+            self.up = False
+            self.down = False
+
+        # print("right", self.right, "left", self.left, "up", self.up, "down", self.down, "dx", dx, "dy", dy)
+
+    def draw(self, win, frames=3):
+        super().draw(win, frames)
+        self.set_hitbox(15, 10, 576//9 - 35, 256//4 - 10)
+        pygame.draw.rect(win, (255, 0, 0), self.hitbox, 2)
--- tempCodeRunnerFile.py
+++ tempCodeRunnerFile.py
@@ -1,1 +1,1 @@
-chase
+player
